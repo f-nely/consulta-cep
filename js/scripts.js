3ff5acd5d0dc00d7e54e583e7128aeada631ce34
nely de faria <nelly.nf54@gmail.com>
2021-09-06 19:17:39 -0300

    Criando evento

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ let estado = document.querySelector('#estado');
 
 cep.value = '01001000';
 
+/* events */
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value;
+    let script = document.createElement('script');
+    script.src = `https://viacep.com.br/ws/${cep}/json/?callback=popularForm`;
+    document.body.appendChild(script);
+});
+
+
