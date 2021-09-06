927c58bd3a60026335d904a08d1160fb1c1eeb16
nely de faria <nelly.nf54@gmail.com>
2021-09-06 19:17:03 -0300

    Adiciona arquivo scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* geral */
+let cep = document.querySelector('#cep');
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado = document.querySelector('#estado');
+
+cep.value = '01001000';
+
