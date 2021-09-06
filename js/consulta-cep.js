e5e179668b52b02cc2c7738ad62a1f1e783deeb4
nely de faria <nelly.nf54@gmail.com>
2021-09-06 19:20:39 -0300

    Adiciona arquivo consulta-cep.js

---
+++ js/consulta-cep.js
@@ -0,0 +1,12 @@
+/* geral */
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let uf = document.querySelector('#estado');
+let btnCep = document.querySelector('#btn-buscar-cep');
+let listaCep = document.querySelector('#lista-cep');
+
+rua.value = 'Domingos Jose';
+cidade.value = 'Porto Alegre';
+uf.value = 'RS';
+
